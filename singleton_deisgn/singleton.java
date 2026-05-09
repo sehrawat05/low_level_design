7b5c8af49cdcf13c11faf12f64a82362c8c7bb0c
sehrawat05 <khushi.sehrawat5788@gmail.com>
2026-05-09 13:19:51 +0530

    singleton example

---
+++ singleton_deisgn/singleton.java
@@ -0,0 +1,22 @@
+
+class Singleton {
+
+    static private Singleton instance;
+
+    private Singleton() {
+        System.out.println("Singleton instance created");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+    }
+}
